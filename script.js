b686d907a87ba16f9e2b88a7b0725776d55b759a
gbubemi05 <165071754+gbubemi05@users.noreply.github.com>
2024-12-30 12:34:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script for navigation bar
+"use strict";
+const gbubemi = {
+  firstName: "gbubemi",
+  lastname: "williams",
+};
+const marriedGbubemi = gbubemi;
+console.log("Before:", gbubemi);
+console.log("After:", marriedGbubemi);
