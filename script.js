00d09a533d293b9dec70b11e1aea2326bd0170ad
gbubemi05 <165071754+gbubemi05@users.noreply.github.com>
2024-12-31 00:01:40 +0100

    fixed the grid

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // Script for navigation bar
 "use strict";
-const gbubemi = {
-  firstName: "gbubemi",
-  lastname: "williams",
-};
-const marriedGbubemi = gbubemi;
-console.log("Before:", gbubemi);
-console.log("After:", marriedGbubemi);
+const mobileNav = document.querySelector(".nav-bar");
+const mobileOpen = document.querySelector(".bi-list");
+const mobileClose = document.querySelector(".bi-x-lg");
+const overLay = document.querySelector(".overlay");
+mobileOpen.addEventListener("click", function () {
+  mobileNav.classList.add("active");
+  overLay.classList.add("active");
+});
+mobileClose.addEventListener("click", () => {
+  mobileNav.classList.remove("active");
+  overLay.classList.remove("active");
+});
